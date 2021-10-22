bb6d4f9f4a1c3d964e5cc771dd6d0ae3bb30a4ab
ekkakon <emabaciu@gmail.com>
2021-10-22 08:53:41 +0100

    roll back

--- src/masternode-payments.h
+++ src/masternode-payments.h
@@ -22,8 +22,8 @@ class CMasternodeBlockPayees;
 
 extern CMasternodePayments masternodePayments;
 
-#define MNPAYMENTS_SIGNATURES_REQUIRED 2
-#define MNPAYMENTS_SIGNATURES_TOTAL 3
+#define MNPAYMENTS_SIGNATURES_REQUIRED 6
+#define MNPAYMENTS_SIGNATURES_TOTAL 10
 
 void ProcessMessageMasternodePayments(CNode* pfrom, std::string& strCommand, CDataStream& vRecv);
 bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight);
