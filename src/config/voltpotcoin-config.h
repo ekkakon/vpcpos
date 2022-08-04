cc9cd0628a07c51cd9f376b71f36077f7b2b3c46
ekkakon <emabaciu@gmail.com>
2022-08-04 18:22:50 +0100

    version update

--- src/config/voltpotcoin-config.h
+++ src/config/voltpotcoin-config.h
@@ -18,13 +18,13 @@
 #define CLIENT_VERSION_MAJOR 1
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 5
+#define CLIENT_VERSION_REVISION 0
 
 /* Version is release */
-#define COPYRIGHT_YEAR 2021
+#define COPYRIGHT_YEAR 2022
 
 /* Define this symbol to build code that uses AVX2 intrinsics */
 #define ENABLE_AVX2 1
