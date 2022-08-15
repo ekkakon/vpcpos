e78d53790a6e56f05f58d877d67a9d2a6ebbc4cc
ekkakon <emabaciu@gmail.com>
2022-08-15 14:19:18 +0100

    CzVPC fix

--- src/test/zerocoin_implementation_tests.cpp
+++ src/test/zerocoin_implementation_tests.cpp
@@ -486,7 +486,7 @@ BOOST_AUTO_TEST_CASE(deterministic_tests)
     for (int i = 0; i < nTests; i++) {
         libzerocoin::PrivateCoin coin(Params().Zerocoin_Params(false), denom, false);
         CDeterministicMint dMint;
-        zWallet.GenerateDeterministicZPIV(denom, coin, dMint);
+        zWallet.GenerateDeterministicZVPC(denom, coin, dMint);
         vCoins.emplace_back(coin);
     }
 
