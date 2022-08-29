02a65e3f08ea6c5128f482cef8e040725ec78b63
ekkakon <emabaciu@gmail.com>
2022-08-29 17:50:24 +0100

    typo fix

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -920,10 +920,10 @@ CAmount CBudgetManager::GetTotalBudget(int nHeight)
     if (nHeight == 0) {
      nSubsidy = 746910 * COIN;
     }
-    else if (nHeight <= Params().LAST_POW_BLOCK() && nHeight > 0) {
+    else if (nHeight <= last_pow_block && nHeight > 0) {
      nSubsidy = 10 * COIN;
     }
-    else if (nHeight > Params().LAST_POW_BLOCK()) {
+    else if (nHeight > last_pow_block) {
      nSubsidy = 10 * COIN;
     }
 
