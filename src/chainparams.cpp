1fad90a1a83f445ac2433e9a20cac47f71f6778a
ekkakon <emabaciu@gmail.com>
2022-09-14 10:47:53 +0100

    miner change for testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -379,7 +379,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
 
-        fMiningRequiresPeers = false;
+        fMiningRequiresPeers = true;
         fAllowMinDifficultyBlocks = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
