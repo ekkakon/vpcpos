3a4cde0bf4c92190c2be57a68aea3a5f8226343e
ekkakon <emabaciu@gmail.com>
2022-09-18 17:21:43 +0100

    fake serials end height

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -148,7 +148,7 @@ public:
         nPublicZCSpends = 1;
 
         // Fake Serial Attack
-        nFakeSerialBlockheightEnd = 0;
+        nFakeSerialBlockheightEnd = 1;
         nSupplyBeforeFakeSerial = 568897;   // zerocoin supply at block nFakeSerialBlockheightEnd
 
         /**
