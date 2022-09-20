e50fcfb78ea32e769b3618afbe955927d9ebafea
ekkakon <emabaciu@gmail.com>
2022-09-20 09:27:05 +0100

    ZC V2 starta and acc recalc start block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -150,12 +150,12 @@ public:
         nZerocoinStartHeight = 1;
         nZerocoinStartTime = 1623799585;
         nBlockEnforceSerialRange = 4000; //Enforce serial range starting this block
-        nBlockRecalculateAccumulators = 571695; //Trigger a recalculation of accumulators
+        nBlockRecalculateAccumulators = 572000; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = 1; //First block that bad serials emerged
         nBlockLastGoodCheckpoint = 0; //Last valid accumulator checkpoint
         nBlockEnforceInvalidUTXO = 11000; //Start enforcing the invalid UTXO's
         nInvalidAmountFiltered = 0; //Amount of invalid coins filtered through exchanges, that should be considered valid
-        nBlockZerocoinV2 = 572000;
+        nBlockZerocoinV2 = 572500;
         nBlockDoubleAccumulated = 999999999;
         nEnforceNewSporkKey = 1623799585; //!> Sporks signed after (GMT): Tuesday, May 1, 2018 7:00:00 AM GMT must use the new spork key
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM
