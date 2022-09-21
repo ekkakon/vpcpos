3d423affeca2f3aafdffafc27fedf888bc38e90f
ekkakon <emabaciu@gmail.com>
2022-09-21 12:10:26 +0100

    ZC start and V2

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -132,16 +132,16 @@ public:
 
         /** Height or Time Based Activations **/
         nLastPOWBlock = 1000;
-        nModifierUpdateBlock = 0;
+        nModifierUpdateBlock = 1;
         nZerocoinStartHeight = 1;
-        nZerocoinStartTime = 1623799585;
+        nZerocoinStartTime = 1624830210;
         nBlockEnforceSerialRange = 1; //Enforce serial range starting this block
         nBlockRecalculateAccumulators = 999999999; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = 999999999; //First block that bad serials emerged
         nBlockLastGoodCheckpoint = 999999999; //Last valid accumulator checkpoint
         nBlockEnforceInvalidUTXO = 999999999; //Start enforcing the invalid UTXO's
         nInvalidAmountFiltered = 0; //Amount of invalid coins filtered through exchanges, that should be considered valid
-        nBlockZerocoinV2 = 1001;
+        nBlockZerocoinV2 = 999999999;
         nBlockDoubleAccumulated = 999999999;
         nEnforceNewSporkKey = 1623799585;
         nRejectOldSporkKey = 1527811200;
