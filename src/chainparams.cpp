1ae2167c52bf879ed24bf8bbeb2cc4e531a937ed
ekkakon <emabaciu@gmail.com>
2022-09-21 10:32:09 +0100

    trigger recalc

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,7 @@ public:
         nModifierUpdateBlock = 1;
         nZerocoinStartHeight = 1;
         nZerocoinStartTime = 1623799585;
-        nBlockEnforceSerialRange = 999999999; //Enforce serial range starting this block
+        nBlockEnforceSerialRange = 1; //Enforce serial range starting this block
         nBlockRecalculateAccumulators = 572999; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = 999999999; //First block that bad serials emerged
         nBlockLastGoodCheckpoint = 1; //Last valid accumulator checkpoint
